489e8f8815b66bf885c1b723efd4be9deaf4630c
Evaaaaaa <evazyabrevaaa@gmail.com>
2022-02-23 15:49:02 +0200

    refactor object manipulators

--- Assets/Scripts/ObjectMover.cs
+++ Assets/Scripts/ObjectMover.cs
@@ -1,21 +1,11 @@
 using UnityEngine;
 
-public class ObjectMover : MonoBehaviour
+public class ObjectMover : ObjectManipulatorBase
 {
     public Vector3 Offset = new Vector3(0, 0, 5f);
-    public float ReverseCycleDuration = 3f;
-    private float timer = 0f;
-    private float directionMultipicator = 1f;
-
-    private void Update()
+   
+    public override void Manipulate(float directionMultiplicator)
     {
-        timer += Time.deltaTime;
-        if (timer >= ReverseCycleDuration)
-        {
-            directionMultipicator *= -1f;
-            timer -= ReverseCycleDuration;
-        }
-        
-        transform.position += Offset * directionMultipicator * Time.deltaTime;
+        transform.position += Offset * directionMultiplicator * Time.deltaTime;
     }
 }
--- Assets/Scripts/ObjectRotator.cs
+++ Assets/Scripts/ObjectRotator.cs
@@ -1,20 +1,11 @@
 using UnityEngine;
 
-public class ObjectRotator : MonoBehaviour
+public class ObjectRotator : ObjectManipulatorBase
 {
     public Vector3 RotatePerSecond = new Vector3(0f, 30f, 0f);
-    public float ReverseCycleDuration = 3f;
-    private float timer = 0f;
-    private float directionMultiplicator = 1f;
 
-    private void Update()
+    public override void Manipulate(float directionMultiplicator)
     {
-        timer += Time.deltaTime;
-        if(timer >= ReverseCycleDuration)
-        {
-            directionMultiplicator *= -1f;
-            timer -= ReverseCycleDuration;
-        }
         transform.Rotate(RotatePerSecond * directionMultiplicator * Time.deltaTime);
     }
 }
