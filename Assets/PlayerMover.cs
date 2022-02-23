36417451250bc7eddf6d7401b1bd6256d02fb362
Evaaaaaa <evazyabrevaaa@gmail.com>
2022-02-23 11:57:19 +0200

    move change

--- Assets/PlayerMover.cs
+++ Assets/PlayerMover.cs
@@ -9,8 +9,7 @@ public class PlayerMover : MonoBehaviour
     void Update()
     {
         Vector3 offset = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-        Vector3 forwardSpeedSet = new Vector3(0, 0, ForwardSpeed);
-        transform.position += forwardSpeedSet.normalized * Time.deltaTime * ForwardSpeed;
-        transform.position += offset.normalized * Time.deltaTime * PlayerSpeed;
+        transform.position += new Vector3(0, 0, ForwardSpeed) * Time.deltaTime;
+        transform.position += offset.normalized * PlayerSpeed * Time.deltaTime;
     }
 }
