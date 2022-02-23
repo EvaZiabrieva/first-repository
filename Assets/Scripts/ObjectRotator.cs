731f22e229844695ff8a8864dc5f3347df49c167
Evaaaaaa <evazyabrevaaa@gmail.com>
2022-02-23 15:34:37 +0200

    make object rotator cyclic

--- Assets/Scripts/ObjectRotator.cs
+++ Assets/Scripts/ObjectRotator.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class ObjectRotator : MonoBehaviour
 {
     public Vector3 RotatePerSecond = new Vector3(0f, 30f, 0f);
+    public float ReverseCycleDuration = 3f;
+    private float timer = 0f;
+    private float directionMultiplicator = 1f;
 
     private void Update()
     {
-        transform.Rotate(RotatePerSecond * Time.deltaTime);
+        timer += Time.deltaTime;
+        if(timer >= ReverseCycleDuration)
+        {
+            directionMultiplicator *= -1f;
+            timer -= ReverseCycleDuration;
+        }
+        transform.Rotate(RotatePerSecond * directionMultiplicator * Time.deltaTime);
     }
 }
