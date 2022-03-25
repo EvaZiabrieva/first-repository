d77536f5770b82bd7b6e8e887f141f031bf0e947
Evaaaaaa <evazyabrevaaa@gmail.com>
2022-03-25 18:58:08 +0200

    Add vertical rotation

--- Assets/FPS/Scripts/PlayerMovementController.cs
+++ Assets/FPS/Scripts/PlayerMovementController.cs
@@ -5,9 +5,13 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private Transform _camera;
     [SerializeField, Min(0f)] private float _rotationHorizontalSensetivity = 10f;
+    [SerializeField, Min(0f)] private float _rotationVerticalSensetivity = 10f;
+    [SerializeField, Range(-90f, 90f)] private float _minVericalAngle = -50f;
+    [SerializeField, Range(-90f, 90f)] private float _maxVericalAngle = 50f;
 
     private void FixedUpdate()
     {
         _rigidbody.MoveRotation(Quaternion.Euler(0f, _rotationHorizontalSensetivity * Input.mousePosition.x, 0f));
+        _camera.localRotation = Quaternion.Euler(Mathf.Clamp(-_rotationVerticalSensetivity * Input.mousePosition.y, _minVericalAngle, _maxVericalAngle), 0f, 0f);
     }
 }
